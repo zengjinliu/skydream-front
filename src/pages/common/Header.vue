77506c037b6739a5618d403f533cb5018324a8b3
zengjinliu <zengjin_job@163.com>
2020-09-08 08:31:03 +0800

    页面整体布局

---
+++ src/pages/common/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header">
+      <p>this is header</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+  .header{
+    border-bottom: 1px solid #f6f6f6;
+    box-sizing: border-box;
+    background-color: grey;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    display: block;
+    z-index: 1000px;
+  }
+</style>
